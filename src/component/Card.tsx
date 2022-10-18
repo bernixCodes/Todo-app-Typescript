475401a8e1193c81b7272850aba0bc5987da3184
bernice <bernice@hubtel.com>
2022-10-18 20:02:44 +0000

    delete and edit functionality

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -9,17 +9,31 @@ interface Props {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 const Card = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
   return (
     <form className="flex items-center justify-between w-full mx-5  md:w-1/3 p-4 my-2 text-white bg-btnColor">
-      <span className="flex-1">{todo.todo}</span>
-      <div className="flex gap-2">
+      {todo.isDone ? (
+        <span className="flex-1 line-through">{todo.todo}</span>
+      ) : (
+        <span className="flex-1">{todo.todo}</span>
+      )}
+      <div className="flex gap-2 text-lg cursor-pointer">
         <span>
           <AiFillEdit />
         </span>
-        <span>
+        <span onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
-        <span>
+        <span onClick={() => handleDone(todo.id)}>
           <MdDone />
         </span>
       </div>
